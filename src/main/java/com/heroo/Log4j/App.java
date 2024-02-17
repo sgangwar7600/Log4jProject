00ee91039e5eda6cae60d1e23ca01aabf7dd02d4
sgangwar7600 <shashikantkant26@gmail.com>
2024-02-17 09:18:30 +0530

    log4jproject

---
+++ src/main/java/com/heroo/Log4j/App.java
@@ -0,0 +1,13 @@
+package com.heroo.Log4j;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Demo/LoggerDemo.java
@@ -0,0 +1,21 @@
+package Demo;
+
+import org.apache.logging.log4j.*;
+
+//import com.sun.tools.sjavac.Log;
+
+public class LoggerDemo {
+
+	public static void main(String[] args) {}
+	
+	Logger log = LogManager.getLogger("LoggerDemo");
+	
+	//log.info("for info only");
+	//log.debug("for debug");
+	//log.error("error message");
+	//log.warn("warning message");
+	
+	
+	
+	
+}
